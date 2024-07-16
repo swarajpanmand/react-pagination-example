2abbb1922ff78d4cf67d9b3057fa1f12817dd510
Swaraj Panmand <swarajpanmand@gmail.com>
2024-07-17 01:03:45 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import "./App.css";
 
-function App() {
+const itemsPerPage = 5;
+
+const paginationReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_PAGE":
+      return { ...state, currentPage: action.payload };
+    case "SET_TOTAL_ITEMS":
+      return { ...state, totalItems: action.payload };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [paginationState, dispatch] = useReducer(paginationReducer, {
+    currentPage: 1,
+    totalItems: 0,
+  });
+
+  const data = Array.from({ length: 25 }, (_, index) => `Item ${index + 1}`);
+
+  useEffect(() => {
+    dispatch({ type: "SET_TOTAL_ITEMS", payload: data.length });
+  }, [data]);
+
+  const startIndex = (paginationState.currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const displayedItems = data.slice(startIndex, endIndex);
+
+  const handlePageClick = (newPage) => {
+    dispatch({ type: "SET_CURRENT_PAGE", payload: newPage });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="pagination-container">
+      <h1 className="pagination-title">Pagination Example</h1>
+      <ul className="item-list">
+        {displayedItems.map((item, index) => (
+          <li key={index} className="item">{item}</li>
+        ))}
+      </ul>
+      <div className="pagination-controls">
+        <button
+          className="pagination-button"
+          onClick={() => handlePageClick(paginationState.currentPage - 1)}
+          disabled={paginationState.currentPage === 1}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <span className="page-info">
+          Page {paginationState.currentPage} of {Math.ceil(data.length / itemsPerPage)}
+        </span>
+        <button
+          className="pagination-button"
+          onClick={() => handlePageClick(paginationState.currentPage + 1)}
+          disabled={endIndex >= data.length}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
